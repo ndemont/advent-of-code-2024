f4fdfd8b17910f06b3a3af7885d0c616141ffbba
Nastasia Demont <nastasia.demont@doctolib.com>
2024-12-01 13:24:53 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,61 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+        var sum: Int = 0
+
+        for (line in input) {
+            val lines: List<String> = line.split("\\s+".toRegex()).filter { it.isNotEmpty() }
+
+            leftList.add(lines[0].toInt())
+            rightList.add(lines[1].toInt())
+        }
+
+        while (leftList.isNotEmpty()) {
+            val leftMinWithIndex = leftList.withIndex().minByOrNull { it.value }
+            val leftMin: Int = leftMinWithIndex?.value ?: 0
+            val leftMinIndex: Int = leftMinWithIndex?.index ?: -1
+
+            val rightMinWithIndex = rightList.withIndex().minByOrNull { it.value }
+            val rightMin: Int = rightMinWithIndex?.value ?: 0
+            val rightMinIndex: Int = rightMinWithIndex?.index ?: -1
+
+            sum += abs(leftMin - rightMin)
+
+            leftList.removeAt(leftMinIndex)
+            rightList.removeAt(rightMinIndex)
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
+        var sum: Int = 0
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        for (line in input) {
+            val lines: List<String> = line.split("\\s+".toRegex()).filter { it.isNotEmpty() }
+
+            leftList.add(lines[0].toInt())
+            rightList.add(lines[1].toInt())
+        }
+
+        for (number in leftList) {
+            val multiplicative: Int = rightList.count { it == number }
+
+            sum += (number * multiplicative)
+        }
+
+        return sum
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput).println()
+    part2(testInput).println()
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
