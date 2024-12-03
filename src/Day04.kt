1b91c1be8f13cc8c76fade387bccba05ccbc414b
Nastasia Demont <nastasia.demont@doctolib.com>
2024-12-03 10:14:06 +0100

    prepare Day04

---
+++ src/Day04.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day04_test")
+    println("Test output (part1): ${part1(testInput)}")
+    println("Test output (part2): ${part2(testInput)}")
+
+    val realInput = readInput("Day04")
+    println("Real output (part1): ${part1(realInput)}")
+    println("Real output (part2): ${part2(realInput)}")
+}
