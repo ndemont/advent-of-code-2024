f021263c86ba3ae45c29f8b1fd8656b59da7ebc5
Nastasia Demont <nastasia.demont@doctolib.com>
2024-12-05 14:30:54 +0100

    Day04 part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,14 +1,9 @@
 fun main() {
     fun xmasCount(line: String): Int {
-        println(line)
-        println("XMAS = ${Regex("XMAS").findAll(line).count()}")
-        println("SAMX = ${Regex("SAMX").findAll(line).count()}")
-        println("")
-
         return Regex("XMAS").findAll(line).count() + Regex("SAMX").findAll(line).count()
     }
 
-    fun getDiagonal(input: List<String>, xPos: Int, yUp: Int, yDown: Int): Pair<String, String> {
+    fun getDiagonals(input: List<String>, xPos: Int, yUp: Int, yDown: Int): Pair<String, String> {
         var diagonalUp = ""
         var diagonalDown = ""
         var yUpPos = yUp
@@ -33,44 +28,72 @@ fun main() {
         var xmasCount: Int = 0
 
         for ((index, line) in input.withIndex()) {
-            println("Y = $index")
-            val (diagonalUp, diagonalDown) = getDiagonal(input, 0, index, index)
-            println("diagonalUp")
+            val (diagonalUp, diagonalDown) = getDiagonals(input, 0, index, index)
             xmasCount += xmasCount(diagonalUp)
-            println("diagonalDown")
             xmasCount += xmasCount(diagonalDown)
-            println("horizontal")
             xmasCount += xmasCount(line)
         }
 
         for (i in input[0].indices)  {
             val horizontalLine = input.map { it[i] }.joinToString("")
-            println("X = $i")
-            println("vertical")
             xmasCount += xmasCount(horizontalLine)
-
             if (i > 0) {
-                val (diagonalUp, diagonalDown) = getDiagonal(input, i, input.size - 1, 0)
-
-                println("diagonalUp")
+                val (diagonalUp, diagonalDown) = getDiagonals(input, i, input.size - 1, 0)
                 xmasCount += xmasCount(diagonalUp)
-                println("diagonalDown")
                 xmasCount += xmasCount(diagonalDown)
+            }
+        }
+        return xmasCount
+    }
+
+
+    fun isXmas(input: List<String>, x: Int, y: Int) : Int {
+        if (x == 0 || y == 0) {
+            return 0
+        }
+
+        if ((x >= input[0].length -1 ) || (y >= input.size - 1)) {
+            return 0
+        }
+
+        val diagonalUp = buildString {
+            append(input[y + 1][x - 1])
+            append(input[y][x])
+            append(input[y - 1][x + 1])
+        }
+
+        val diagonalDown = buildString {
+            append(input[y - 1][x - 1])
+            append(input[y][x])
+            append(input[y + 1][x + 1])
+        }
+
+        if ((diagonalUp == "MAS" || diagonalUp == "SAM") && (diagonalDown == "MAS" || diagonalDown == "SAM")) {
+            return 1
+        }
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        var xmasCount = 0
+
+        for ((y, line) in input.withIndex()) {
+            for (x in line.indices) {
+                if (line[x] == 'A') {
+                    xmasCount += isXmas(input, x, y)
+                }
             }
         }
 
         return xmasCount
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-
     val testInput = readInput("Day04_test")
     println("Test output (part1): ${part1(testInput)}")
-//    println("Test output (part2): ${part2(testInput)}")
-//
+    println("Test output (part2): ${part2(testInput)}")
+
     val realInput = readInput("Day04")
     println("Real output (part1): ${part1(realInput)}")
-//    println("Real output (part2): ${part2(realInput)}")
+    println("Real output (part2): ${part2(realInput)}")
 }
