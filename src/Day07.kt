b4a19c859598e7ff23582106efacf4c417948c2a
Nastasia Demont <nastasia.demont@doctolib.com>
2024-12-07 15:22:36 +0100

    improve perf: 81 ms

--- src/Day07.kt
+++ src/Day07.kt
@@ -11,14 +11,33 @@ fun main() {
         return 10.0.pow(getDigitCount(number)).toInt()
     }
 
-    fun testCalibration(target: Long, index: Int, current: Long, numbers: List<Int>): Boolean {
+    fun calibrate1(target: Long, index: Int, current: Long, numbers: List<Int>): Boolean {
         val newIndex = index + 1
         if (newIndex == numbers.size) return target == current
 
         val number = numbers[newIndex]
-        return testCalibration(target, newIndex, (current * number), numbers)
-                || testCalibration(target, newIndex, (current + number), numbers)
-                || testCalibration(target, newIndex, (current * getPowerOfTen(number)) + number, numbers)
+        return calibrate1(target, newIndex, (current * number), numbers)
+                || calibrate1(target, newIndex, (current + number), numbers)
+    }
+
+    fun calibrate2(target: Long, index: Int, current: Long, numbers: List<Int>): Boolean {
+        val newIndex = index + 1
+        if (newIndex == numbers.size) return target == current
+
+        val number = numbers[newIndex]
+        return calibrate2(target, newIndex, (current * number), numbers)
+                || calibrate2(target, newIndex, (current + number), numbers)
+                || calibrate2(target, newIndex, (current * getPowerOfTen(number)) + number, numbers)
+    }
+
+    fun part1(input: List<String>): Long {
+        return input.sumOf { equation ->
+            val (resultPart, testValuesPart) = equation.split(":").map { it.trim() }
+            val result = resultPart.toLong()
+            val numbers = testValuesPart.split(" ").map { it.toInt() }
+
+            if (calibrate1(result, 0, numbers[0].toLong(), numbers)) result else 0
+        }
     }
 
     fun part2(input: List<String>): Long {
@@ -27,16 +46,16 @@ fun main() {
             val result = resultPart.toLong()
             val numbers = testValuesPart.split(" ").map { it.toInt() }
 
-            if (testCalibration(result, 0, numbers[0].toLong(), numbers)) result else 0
+            if (calibrate2(result, 0, numbers[0].toLong(), numbers)) result else 0
         }
     }
 
-//    val testInput = readInput("Day07_test")
-//    println("Test output (part1): ${part1(testInput)}")
-//    println("Test output (part2): ${part2(testInput)}")
+    val testInput = readInput("Day07_test")
+    println("Test output (part1): ${part1(testInput)}")
+    println("Test output (part2): ${part2(testInput)}")
 
     val realInput = readInput("Day07")
-//    println("Real output (part1): ${part1(realInput)}")
+    println("Real output (part1): ${part1(realInput)}")
     println("Real output (part2): ${part2(realInput)}")
 
     val endTime = System.currentTimeMillis()
