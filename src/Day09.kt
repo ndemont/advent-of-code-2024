f42eec061d82bf8f9eda8a5156d60c958a3b5c15
Nastasia Demont <nastasia.demont@doctolib.com>
2024-12-09 15:13:04 +0100

    refactor Day9

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,74 +1,70 @@
 fun main() {
     val startTime = System.currentTimeMillis()
 
-    fun moveFiles(filesToMove: MutableList<Char>, filesInFront: MutableList<Char>): Pair<MutableList<Char>, MutableList<Char>> {
-        return filesInFront to filesToMove
-    }
-
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val diskMap: MutableList<String> = mutableListOf()
-        var countinit = 0
-
 
         input.first().forEachIndexed { index, blocks ->
-            if (index % 2 == 0) {
-                val newFile = (index / 2).toString().repeat(blocks.toString().toInt())
-                countinit += (blocks.toString().toInt() * (index / 2).toString().length)
-                diskMap.add(newFile)
-            } else {
-                val newFile = '.'.toString().repeat(blocks.toString().toInt())
-                diskMap.add(newFile)
-            }
+            val newFile = if (index % 2 == 0) (index / 2).toString() else "."
+            repeat(blocks.toString().toInt()) { diskMap.add(newFile) }
         }
 
-        val diskMapChar: MutableList<Char> = diskMap.flatMap { it.toList() }.toMutableList()
-        var files = diskMapChar.reversed()
         var dotIndex = 0
-        var countref = 0
-        var countresult = 0
-
-        files.forEachIndexed() { index, file ->
-            if (file != '.') {
-                countref++
-                while (dotIndex < diskMapChar.size && diskMapChar[dotIndex] != '.') {
+        diskMap.reversed().forEachIndexed { reverseIndex, file ->
+            if (file != ".") {
+                while (dotIndex < diskMap.size && diskMap[dotIndex] != ".") {
                     dotIndex++
                 }
 
-                if (dotIndex < files.size - 1 - index) {
-                    diskMapChar[dotIndex] = file
-                    diskMapChar[files.size - 1 - index] = '.'
+                if (dotIndex < diskMap.size - 1 - reverseIndex) {
+                    diskMap[dotIndex] = file
+                    diskMap[diskMap.size - 1 - reverseIndex] = "."
                     dotIndex++
-                    println("last dot index: $dotIndex, last char: $file, last char index: ${files.size -1 - index}")
                 }
             }
         }
 
-        println(diskMap)
-        println(diskMapChar)
-        var sum = 0
-        diskMapChar.forEachIndexed() { index, char ->
-            if (char != '.')  {
-                countresult++
-                sum += (char.toString().toInt() * index)
-            }
-        }
-
-        println("$countref == $countresult == $countinit")
-
-        return sum
+        return diskMap.mapIndexedNotNull { index, file ->
+            file.toIntOrNull()?.times(index)
+        }.sumOf { it.toLong() }
     }
 
-//    fun part2(input: List<String>): Int {
-//
-//    }
+    fun part2(input: List<String>): Long {
+        val diskMap: MutableList<MutableList<String>> = mutableListOf()
+
+        input.first().forEachIndexed { index, blocks ->
+            val newFile = if (index % 2 == 0) (index / 2).toString() else "."
+            val newFileList = List(blocks.toString().toInt()) { newFile }
+            if (newFileList.isNotEmpty()) { diskMap.add(newFileList.toMutableList()) }
+        }
+
+        diskMap.reversed().forEachIndexed { reverseIndex, files ->
+            if (files.first() != ".") {
+                val availableSpotIndex = diskMap.indexOfFirst { block -> block.count { it == "." } >= files.size }
+
+                if (availableSpotIndex != -1 && availableSpotIndex < diskMap.size - reverseIndex) {
+                    val startEmptyIndex = diskMap[availableSpotIndex].indexOfFirst { it == "." }
+
+                    files.forEachIndexed { i, file ->
+                        diskMap[availableSpotIndex][startEmptyIndex + i] = file
+                        diskMap[diskMap.size - 1 - reverseIndex][i] = "."
+                    }
+                }
+            }
+        }
+
+        return diskMap.flatten().mapIndexedNotNull { index, file ->
+            file.toIntOrNull()?.times(index)
+        }.sumOf { it.toLong() }
+    }
 
     val testInput = readInput("Day09_test")
     println("Test output (part1): ${part1(testInput)}")
-//    println("Test output (part2): ${part2(testInput)}")
-//
+    println("Test output (part2): ${part2(testInput)}")
+
     val realInput = readInput("Day09")
-//    println("Real output (part1): ${part1(realInput)}")
-//    println("Real output (part2): ${part2(realInput)}")
+    println("Real output (part1): ${part1(realInput)}")
+    println("Real output (part2): ${part2(realInput)}")
 
     val endTime = System.currentTimeMillis()
     println("Execution time: ${endTime - startTime} ms")
